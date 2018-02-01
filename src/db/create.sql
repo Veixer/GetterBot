e5596a94563cb61a0824b7f20bd0be8e8615189a
Veixer <Veixer@users.noreply.github.com>
2018-02-01 23:34:50 +0200

    Fixed order

--- src/db/create.sql
+++ src/db/create.sql
@@ -4,26 +4,6 @@ USE [database_name]
 SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 
-CREATE TABLE [dbo].[botget](
-	[messageid] [int] NOT NULL,
-	[userid] [int] NULL,
-	[get_date] [datetime] NULL,
-	[get_seconds] [int] NULL,
-	[get_weekday] [nvarchar](20) NULL,
-	[get_type_id] [int] NULL,
-	[chatid] [int] NULL,
-	[chattitle] [nvarchar](150) NULL,
-    PRIMARY KEY CLUSTERED ([messageid] ASC)
-    WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-)
-ON [PRIMARY]
-
-ALTER TABLE [dbo].[botget]  WITH CHECK ADD FOREIGN KEY([get_type_id])
-REFERENCES [dbo].[get_type] ([id])
-
-ALTER TABLE [dbo].[botget]  WITH CHECK ADD FOREIGN KEY([userid])
-REFERENCES [dbo].[users] ([userid])
-
 CREATE TABLE [dbo].[get_type](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[get_type_name] [nvarchar](10) NULL,
@@ -48,3 +28,23 @@ CREATE TABLE [dbo].[users](
     WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 )
 ON [PRIMARY]
+
+CREATE TABLE [dbo].[botget](
+	[messageid] [int] NOT NULL,
+	[userid] [int] NULL,
+	[get_date] [datetime] NULL,
+	[get_seconds] [int] NULL,
+	[get_weekday] [nvarchar](20) NULL,
+	[get_type_id] [int] NULL,
+	[chatid] [int] NULL,
+	[chattitle] [nvarchar](150) NULL,
+    PRIMARY KEY CLUSTERED ([messageid] ASC)
+    WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)
+ON [PRIMARY]
+
+ALTER TABLE [dbo].[botget]  WITH CHECK ADD FOREIGN KEY([get_type_id])
+REFERENCES [dbo].[get_type] ([id])
+
+ALTER TABLE [dbo].[botget]  WITH CHECK ADD FOREIGN KEY([userid])
+REFERENCES [dbo].[users] ([userid])
